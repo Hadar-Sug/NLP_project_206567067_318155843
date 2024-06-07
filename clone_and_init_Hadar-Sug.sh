2d98a4f36d194cfdcbbf8ee4cf4361809e573af0
Hadar-Sug <81767379+Hadar-Sug@users.noreply.github.com>
2024-06-07 14:08:20 +0300

    Update clone_and_init_Hadar-Sug.sh

--- clone_and_init_Hadar-Sug.sh
+++ clone_and_init_Hadar-Sug.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Remove the directory and all its contents
-rm -rf /path/to/directory
-
 # Clone the repository and set up the project
 git clone https://github.com/Hadar-Sug/NLP_project_206567067_318155843.git
 cd NLP_project_206567067_318155843

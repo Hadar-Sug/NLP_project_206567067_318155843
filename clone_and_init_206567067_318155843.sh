465be1cb5e5877fae76d6518348ad3ec53bb5ae7
Hadar-Sug <hadar.sug@campus.technion.ac.il>
2024-06-07 13:08:44 +0300

    added shell files

---
+++ clone_and_init_206567067_318155843.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Remove the directory and all its contents
+rm -rf /path/to/directory
+
+# Clone the repository and set up the project
+git clone https://github.com/yourusername/NLP_project_206567067_318155843.git
+cd NLP_project_206567067_318155843
+pip install -r requirements.txt
+python main.py

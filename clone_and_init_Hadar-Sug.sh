31374acc1cf13173ce2c59f0988cc5a464ee8b13
Hadar-Sug <81767379+Hadar-Sug@users.noreply.github.com>
2024-06-07 13:47:01 +0300

    Update clone_and_init_Hadar-Sug.sh

--- clone_and_init_Hadar-Sug.sh
+++ clone_and_init_Hadar-Sug.sh
@@ -4,7 +4,7 @@
 rm -rf /path/to/directory
 
 # Clone the repository and set up the project
-git clone https://github.com/yourusername/NLP_project_206567067_318155843.git
+git clone https://github.com/Hadar-Sug/NLP_project_206567067_318155843.git
 cd NLP_project_206567067_318155843
 pip install -r requirements.txt
 python main.py
